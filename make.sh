6ce8554bdd50b912d1c6ce45f153aa919bb50574
Cezarijus Kivylius <1041335+Kivylius@users.noreply.github.com>
2025-05-29 10:41:21 +0100

    update make script to new format

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # make.sh – OpenDartboard 3-cam scorer (Pi Zero 2 W)
 
-export PKG_VERSION=0.3.0
+export PKG_VERSION=0.4.0
 export PKG_NAME=opendartboard
 WIDTH=${WIDTH:-640}
 HEIGHT=${HEIGHT:-480}
@@ -15,17 +15,9 @@ cp /usr/local/share/opendartboard/models/*.bin   ${PKG_NAME}_${PKG_VERSION}/usr/
 
 # Minimal DEBIAN/control file
 mkdir -p ${PKG_NAME}_${PKG_VERSION}/DEBIAN
-cat >${PKG_NAME}_${PKG_VERSION}/DEBIAN/control <<EOF
-Package: ${PKG_NAME}
-Version: ${PKG_VERSION}-1
-Section: misc
-Priority: optional
-Architecture: arm64
-Maintainer: opendartboard community <many>
-Depends: libopencv-dev, gstreamer1.0-libav
-Description: Headless 3-camera dart scorer for Pi Zero 2 W
- Prints D20|T20|MISS to stdout using YOLOv8-nano + ncnn.
-EOF
+envsubst < distributions/debian_arm64/control > ${PKG_NAME}_${PKG_VERSION}/DEBIAN/control
+envsubst < distributions/debian_arm64/postinst > ${PKG_NAME}_${PKG_VERSION}/DEBIAN/postinst
+chmod +x ${PKG_NAME}_${PKG_VERSION}/DEBIAN/postinst
 
 # Generate service files from templates
 envsubst < templates/opendartboard.service.template > opendartboard.service
@@ -37,13 +29,7 @@ install -Dm644 opendartboard.service \
 install -Dm644 lock_cams.service \
      ${PKG_NAME}_${PKG_VERSION}/lib/systemd/system/lock_cams.service
 
-fuck_yiou
-
 # 4. Build the .deb
 rm -f /app/*.deb
 dpkg-deb --build ${PKG_NAME}_${PKG_VERSION}
 echo "==> Finished build: ${PKG_NAME}_${PKG_VERSION}"
-echo "PWD: $(pwd)"
-ls -lh
-ls -lh /app
-find / -name "*.deb" 2>/dev/null
